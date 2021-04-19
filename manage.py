21982a2c65373730cefe09679dc924822563477d
Justo <Langat05>
2021-04-19 14:21:58 +0300

    finalize the frontend

--- manage.py
+++ manage.py
@@ -1,7 +1,9 @@
 from app import create_app
 from flask_script import Manager,Server
-
+from  flask_migrate import Migrate, MigrateCommand
 # Creating app instance
+
+
 app = create_app('development')
 
 manager = Manager(app)
